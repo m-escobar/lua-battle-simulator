c72697a3ab946613972b10f52ab49fc191a8c57f
Marcelo Escobar <mers.escobar@gmail.com>
2024-05-23 19:53:58 -0300

    updated monsters and knights

--- definitions/lancelote.lua
+++ definitions/lancelote.lua
@@ -3,9 +3,16 @@ local knights = {}
     knights.name = 'Sir Lancelot'
     knights.description = 'A knight of Camelot'
 
-    knights.health = 8
-    knights.attack = 4
-    knights.defense = 5
-    knights.speed = 6
+    knights.health = 10
+    knights.attack = 6
+    knights.defense = 6
+    knights.speed = 9
+    knights.power = 6
+
+    knights.actions = {
+        'sword_attack',
+        'drink_potion',
+        'dodge'
+    }
 
 return knights
