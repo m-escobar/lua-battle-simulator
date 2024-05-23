eae8628b6398f6df43ae0a498e8969d7fdc9daa3
Marcelo Escobar <mers.escobar@gmail.com>
2024-05-23 19:53:48 -0300

    added player_actions

---
+++ lib/player_actions.lua
@@ -0,0 +1,15 @@
+local player_actions = {}
+
+    player_actions.sword_attack = {
+        description = 'Sword Attack'
+    }
+
+    player_actions.drink_potion = {
+        description = 'Drink a magic potion'
+    }
+
+    player_actions.dodge = {
+        description = 'Dodge from oponent attack'
+    }
+
+return player_actions
