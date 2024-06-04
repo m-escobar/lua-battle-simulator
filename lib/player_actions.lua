4abd2041507c9fc1b7fd7d1108cd7f355efd8c6a
Marcelo Escobar <mers.escobar@gmail.com>
2024-06-04 19:10:07 -0300

    added requirement and execute to player_actions

--- lib/player_actions.lua
+++ lib/player_actions.lua
@@ -3,47 +3,69 @@ local player_actions = {}
     player_actions.sword_attack = {
         id = 'sword_attack',
         description = 'Sword Attack',
-        requirement = 1,
+        requirement = {
+            name = 'sword'
+        },
         execute = function()
-
+            print('running at FN sword_attack')
         end
     }
 
     player_actions.drink_potion = {
         id = 'drink_potion',
         description = 'Drink a magic potion',
-        requirement = nil
+        requirement = {
+            name = 'potion'
+        },
+        execute = function()
+            print('running at FN drink_potion')
+        end
     }
 
     player_actions.dodge = {
         id = 'dodge',
         description = 'Dodge from oponent attack',
-        requirement = nil
+        requirement = nil,
+        execute = function()
+                    print('running at FN dodge')
+        end
     }
 
     player_actions.fire_attack = {
         id = 'fire_attack',
         description = 'Throw a fire attack',
-        requirement = nil
+        requirement = nil,
+        execute = function()
+                    print('running at FN dodge')
+        end
     }
 
     player_actions.tail_hit = {
         id = 'tail_hit',
         description = 'Hit your opponent with the tail',
-        requirement = nil
+        requirement = nil,
+        execute = function()
+                    print('running at FN dodge')
+        end
     }
 
 
     player_actions.foot_hit = {
         id = 'foot_hit',
         description = 'Smash that small ant!',
-        requirement = nil
+        requirement = nil,
+        execute = function()
+                    print('running at FN dodge')
+        end
     }
 
     player_actions.hand_hit = {
             id = 'hand_hit',
             description = 'Puch him!',
-            requirement = nil
+            requirement = nil,
+        execute = function()
+                    print('running at FN dodge')
+        end
     }
 
 return player_actions
